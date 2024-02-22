6129769b9ceda514f2616759fccda5e2bb2bd467
Migimu <mikelgm9@gmail.com>
2024-02-22 01:16:06 +0100

    Browser con canciones

--- controlador/ControladorBrowser.py
+++ controlador/ControladorBrowser.py
@@ -1,5 +1,62 @@
-class my_class(object):
-    pass
+from controlador.gestores.Artists import Artists
+from controlador.gestores.Albums import Albums
+from controlador.gestores.Songs import Songs
+
+
+class ControladorBrowser:
+    
+    def __init__(self):
+        self.artists = Artists()
+        self.albums = Albums()
+        self.songs = Songs()
+      
+    def GET_ALL_COINCIDENCES(self, word):       
+        artists = self.artists.GET_ARTIST_BY_NAME(word)
+        albums = self.albums.GET_ALBUM_BY_NAME(word)
+        songs = self.songs.GET_SONG_BY_NAME(word)
+        
+        return artists, albums, songs
+        
+    def GET_ALL_COINCIDENCES_BY_GENRE(self, word, genre):       
+       pass
+        
+    def GET_ALL_COINCIDENCES_BY_YEAR(self, word, year):       
+        pass
+        
+    def GET_ALL_ARTISTS(self):       
+        pass
+        
+    def GET_ALL_ARTISTS_BY_GENRE(self, username, password):       
+        pass
+        
+    def GET_ALL_ARTISTS_BY_YEAR(self, username):       
+        pass
+        
+    def GET_ALL_ALBUMS(self, username, password):       
+        pass
+        
+    def GET_ALL_ALBUMS_BY_GENRE(self, username, password):       
+        pass
+        
+    def GET_ALL_ALBUMS_BY_YEAR(self, username):       
+        pass
+        
+    def GET_ALL_SONGS(self, username, password):       
+        pass
+        
+    def GET_ALL_SONGS_BY_GENRE(self, username, password):       
+        pass
+        
+    def GET_ALL_SONGS_BY_YEAR(self, username):       
+        pass
+        
+
+        
+
+
+
+
+
 
 
 
--- vista/Browser.py
+++ vista/Browser.py
@@ -1,9 +1,10 @@
-from PySide6.QtWidgets import QCalendarWidget, QComboBox, QHBoxLayout, QLineEdit, QLabel, QPushButton, QGridLayout, QVBoxLayout, QWidget
-from PySide6.QtCore import Qt,  Signal, Slot
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QHBoxLayout, QLineEdit, QLabel, QPushButton, QScrollArea, QVBoxLayout, QWidget
+from PySide6.QtCore import QSize, Qt,  Signal, Slot
+from controlador.ControladorBrowser import ControladorBrowser
 
-from controlador.ControladorLogin import ControladorLogin
-from vista.Dialogs import OPEN_INFORMATION_DIALOG
-
+from controlador.clases.Artist import Artist
+from controlador.clases.Album import Album
 
 class Searchbar(QWidget):
     def __init__(self):
@@ -14,17 +15,23 @@ class Searchbar(QWidget):
 
         layout = QHBoxLayout()
 
-        # le añadimos unas cuantas cajas
+        searchBarInput = QLineEdit()
+        searchBarInput.setFixedHeight(35)
         layout.addWidget(QLineEdit(), 8)
-        # layout.addWidget(Caja("blue"))
-        layout.addWidget(QPushButton(), 1)
-        layout.addWidget(QPushButton(), 1)
+
+        searchButton = QPushButton()
+        searchButton.setFixedSize(QSize(30,30))
+        layout.addWidget(searchButton, 1)
+        
+        filterButton = QPushButton()
+        filterButton.setFixedSize(QSize(30,30))
+        layout.addWidget(filterButton, 1)
 
         # modificamos los márgenes
-        layout.setContentsMargins(0, 0, 0, 0)
+        # layout.setContentsMargins(0, 0, 0, 0)
 
-        # modificamos el espaciado
-        layout.setSpacing(0)                     
+        # # modificamos el espaciado
+        # layout.setSpacing(0)                     
         
         self.setLayout(layout)
 
@@ -33,29 +40,48 @@ class Browser(QWidget):
     def __init__(self, user):
         super().__init__()
         self.user = user
-        # self.cl:ControladorLogin = ControladorLogin()
+        self.cl:ControladorBrowser = ControladorBrowser()
         
         self.resize(600, 400)
         self.setMaximumSize(600, 400)
 
         layout = QVBoxLayout()
 
-        # le añadimos unas cuantas cajas
         layout.addWidget(Searchbar(), 1)
-        # layout.addWidget(Caja("blue"))
-        layout.addWidget(QLabel("No hay nada"), 9, Qt.AlignHCenter)
+        
+        self.scroll = QScrollArea()
+        self.scroll.setWidget(QLabel("No hay nada"))
+        layout.addWidget(self.scroll, 9, Qt.AlignHCenter)
 
-        # modificamos los márgenes
         # layout.setContentsMargins(0, 0, 0, 0)
 
-        # modificamos el espaciado
         layout.setSpacing(0)                     
         
         self.setLayout(layout)
 
 
-    def CREAR(self):
-       pass
+    def BUSCAR(self):      
+       searchResults = self.cl.GET_ALL_COINCIDENCES_BY_GENRE()
+       results = QVBoxLayout()
+       for item in searchResults:
+           results.addWidget(self.BUILD_CARD(item))
+       self.scroll.setWidget(results)
+           
+    def BUILD_CARD(self, item):
+       card = QHBoxLayout()
+       iconPath = ""
+       if isinstance(item, Artist):
+           iconPath = "artist"
+       elif isinstance(item, Album):
+           iconPath = "album"
+       else:
+           iconPath = "song"
+       card.addWidget(QLabel(QIcon(iconPath)), 1)
+       card.addWidget(QLabel(""), 3)
+       card.addWidget(QPushButton("<"), 1)
+       
+       return card
+              
            
     @Slot()
     def closeEvent(self, event):
